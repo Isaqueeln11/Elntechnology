b5faa6daedf7670a95aa947e7d26a6619e5b6e29
Isaque Domingos <124200127+Isaqueeln11@users.noreply.github.com>
2025-02-10 07:18:08 -0300

    h3

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function HomePage() {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-2">
               <img
-                src="Eln technology.png"
+                src="/Eln technology.png"
                 alt="ELN Technology Logo"
                 className="w-16 h-16 object-contain"
               />
