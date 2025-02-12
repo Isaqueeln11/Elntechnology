8e6fbd58cf48c7d534affca56f24b575371b1141
Isaque Domingos <124200127+Isaqueeln11@users.noreply.github.com>
2025-02-12 17:20:49 -0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  resolve: {
+    alias: {
+      'firebase/app': 'firebase/app/dist/index.esm.js',
+      'firebase/firestore': 'firebase/firestore/dist/index.esm.js',
+    },
+  },
 });
