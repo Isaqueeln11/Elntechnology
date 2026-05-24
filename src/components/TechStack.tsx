c85c1f6bdee11b40aaae31d4978e1836041d0963
Isaque Domingos <124200127+Isaqueeln11@users.noreply.github.com>
2026-05-23 21:26:05 -0300

    teste

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Code, Cpu, Wifi, Zap, Wrench, Layers, Activity, Thermometer, Radio, Bluetooth, MessageSquare, Cable, BarChart, Box, Settings } from 'lucide-react';
 
+const techStackPatternBackground = `url("data:image/svg+xml;utf8,${encodeURIComponent(
+  '<svg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><g fill="#159AFD" fill-opacity="0.05"><circle cx="30" cy="30" r="1"/></g></g></svg>'
+)})`;
+
 const TechStack = () => {
   const techCategories = [
     {
@@ -96,7 +100,10 @@ const TechStack = () => {
         </div>
         
         {/* Background grid pattern */}
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%2523159AFD\" fill-opacity=\"0.05\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"1\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-50"></div>
+        <div
+          className="absolute inset-0 opacity-50 pointer-events-none"
+          style={{ backgroundImage: techStackPatternBackground }}
+        ></div>
       </div>
     </section>
   );
